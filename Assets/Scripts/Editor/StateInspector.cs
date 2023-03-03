ebc695a1ce34a63e47ae5a340cf2a5cf5fc7e6e3
Omar Mohammed Sadek <34074405+omar-sadek@users.noreply.github.com>
2023-03-03 03:46:29 +0200

    State Inspector Script

---
+++ Assets/Scripts/Editor/StateInspector.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(State), true)]
+public class StateInspector : AbstractInspector
+{
+    protected override void enable()
+    {
+
+    }
+
+    protected override void getFields()
+    {
+    }
+
+    protected override void getProperties()
+    {
+    }
+
+    protected override void mainUI()
+    {
+        
+    }
+}
